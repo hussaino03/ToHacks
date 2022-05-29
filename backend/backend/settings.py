3e5bf2b6eacc400e23dc56d51160d19a8c6485fc
Tejas Ambhore <tejasam2020@gmail.com>
2022-05-29 17:30:03 +0530

    deployement changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wfm7*1#)w7ydc#8rc_l@0m$y4b=z4#rhh4@%8sp$$$233^wt)s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tohacks22-image-to-speech.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
